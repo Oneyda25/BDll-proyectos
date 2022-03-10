6ec0338aa8c918328fca49e8c65e8b42da262e08
Oneyda25 <80559750+Oneyda25@users.noreply.github.com>
2022-03-09 22:03:46 -0500

    Add files via upload

--- distribuidora/distribuidora/consultas.sql
+++ distribuidora/distribuidora/consultas.sql
@@ -1,17 +1,16 @@
+integrantes: Oneyda Perez
+             Greisy Pistala
 /*1. Llenar la base de datos.
   2. consulta sql sucursales.
 	MOSTRAR, ID_SUCURSALES, ENCARGADO_SUCURSAL, NOMBRE_CLIENTE Y EN PARENTESIS SU IDENTIFICACION, PAIS.*/
 
-SELECT `sucursales`.`id_sucursales` AS ID_SUCURSALES,`sucursales`.`nombre_encargado` AS ENCARGADO_SUCURSAL,
+SELECT `sucursales`.`id_sucursales` AS ID_SUCURSAL,`sucursales`.`nombre_encargado` AS ENCARGADO_SUCURSAL,
 CONCAT_WS( '',"(",`numero_identificacion`,')',' ',`primer_nombre`) AS  NOMBRE_CLIENTE,`pais`.`pais` AS PAIS
 	FROM `sucursales`
 	INNER JOIN `cliente` ON `sucursales`.`id_sucursales` = `cliente`.`id_cliente`
-	INNER JOIN `pais` ON `sucursales`.`id_sucursales` = `pais`.`id_pais`
-
-/*CONCAT_WS() de SQL Server concatena dos o más cadenas en una cadena con un separador.
-  ON filtra un conjunto de resultados 
-  FROM especifica las tablas o consultas que contienen los datos que se usarán en la cláusula SELECT.
-  AS para asignar un alias.*/
+	INNER JOIN `ciudad_sucursal` ON `sucursales`.`id_ciudad_sucursal` = `ciudad_sucursal`.`id_ciudad_sucursal`
+	INNER JOIN `departamentos` ON `ciudad_sucursal`.`id_departamento` = `departamentos`.`id_departamento`
+	INNER JOIN `pais` ON `departamentos`.`id_pais` = `pais`.`id_pais`
 	_________________________________________________________________________________________
 	
 /*3. consulta sql clienes.
@@ -21,8 +20,19 @@ SELECT `cliente`.`id_cliente` AS ID_CLIENTE,
 	`cliente`.`primer_nombre` AS NOMBRE_CLIENTE,
 	COUNT(`sucursales`.`id_sucursales`) AS CANTIDAD_SUCURSAL
 	FROM `cliente`
-	INNER JOIN `sucursales` ON `cliente`.`id_cliente` = `sucursales`.`id_ciudad_sucursal`
-	GROUP BY `sucursales`.`id_sucursales`
+	INNER JOIN `sucursales` ON `cliente`.`id_cliente` = `sucursales`.`id_cliente`
+	GROUP BY `cliente`.`id_cliente`
 	_________________________________________________________________________________________
+
+4. 
 	
-	
+ciudad, departamento, pais, cantidad_sucursales
+
+SELECT `ciudad_sucursal`.`descripcion` AS CIUDAD, `departamentos`.`departamento`,`pais`.`pais`, 
+COUNT(`sucursales`.`id_sucursales`)
+FROM `ciudad_sucursal`
+INNER JOIN `departamentos` ON `ciudad_sucursal`.`id_departamento`=`departamentos`.`id_departamento`
+INNER JOIN `pais` ON `pais`.`id_pais`= `departamentos`.`id_pais`
+LEFT JOIN `sucursales` ON `sucursales`.`id_ciudad_sucursal` = `ciudad_sucursal`.`id_ciudad_sucursal`
+GROUP BY `ciudad_sucursal`.`id_ciudad_sucursal`
+
